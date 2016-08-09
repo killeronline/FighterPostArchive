52bb5849f9b4c970d8cd683df2565352c36688a2
Sathish Phani Kurella <sathishphanikurella@gmail.com>
2016-08-09 23:52:59 +0530

    Update JL.cs

--- JL.cs
+++ JL.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System.Collections;
-using Image = UnityEngine.UI.Image;
+//using Image = UnityEngine.UI.Image;
 
 public class JLife : MonoBehaviour
 {
@@ -8,7 +8,7 @@ public class JLife : MonoBehaviour
     #region PreJ
     // Root Folder -8.0.-19.25 , ROT : 40 45 0 
     // RTS MAIN CAM 0,0,z=-60...For Future Changes
-    
+
     public GameObject RoundRings;
     public int mode;
     public Animator anim;
@@ -20,17 +20,16 @@ public class JLife : MonoBehaviour
     public bool is_alive;
     public Color selectionHighlightColor;
     public bool do_once;
-    public Color[] pColors;        
-    public Transform healthCanvas;
-    public Transform healthDisplay;    
+    public Color[] pColors;    
+    public Transform healthDisplay;
     public Camera myCamera;
     public bool isAlphaChangedForHealthBar;
-    public bool isOnHomeScreen = false ;
+    public bool isOnHomeScreen = false;
     public AudioSource HorseSound;
     private bool adjustedAttackSeperation = false;
     private int cHealth = int.MaxValue;
     private int cMaxHealth = -1;
-    private float height;    
+    private float height;
     private int countUp;
     private int sudIdleCounter;
     private bool instantColoring;
@@ -50,7 +49,7 @@ public class JLife : MonoBehaviour
 
     void Start()
     {
-        mode = 0 ;        
+        mode = 0;
         countUp = 0;
         sudIdleCounter = 0;
         atime = 0f;
@@ -58,12 +57,12 @@ public class JLife : MonoBehaviour
         miniTypes = new int[] { 5, 1, 3, 3, 0, 0, 3 };// IDLE , WALK , ATCK , DEAD , 4, 5, RNGE
         // WALK IS MADE 1 to obtain atleast one good walk mode
         //controlParam = "bmode" ;
-        decay = 3 ;
+        decay = 6;
         is_alive = true;
         do_once = true;
-        height = 1.85f;        
+        height = 1.85f;
         anim = this.GetComponent<Animator>();
-        anim.SetInteger( controlParam , (0*10)+1); // 01 is the idle-1
+        anim.SetInteger(controlParam, (0 * 10) + 1); // 01 is the idle-1
         whenNotInUpdate();
         if (!isOnHomeScreen)
         {
@@ -80,49 +79,49 @@ public class JLife : MonoBehaviour
         }
         else
         {
-            Invoke("onScreenRunner",1f);
+            Invoke("onScreenRunner", 1f);
         }
     }
-    
+
     public bool isStayIdleWhenLoadingBar = false;
 
     public void onScreenRunner()
     {
         onScreenSeconds++;
-        if ( onScreenSeconds % 10 == 0 || isStayIdleWhenLoadingBar )
+        if (onScreenSeconds % 10 == 0 || isStayIdleWhenLoadingBar)
         {
-            mode = 0;           
+            mode = 0;
             onScreenSeconds = 0;
-        }        
+        }
         else
         {
             mode = 1;
-        }        
+        }
         int randomAction = Random.Range(1, miniTypes[mode] + 1);
         anim.SetInteger(controlParam, (mode * 10) + randomAction); // Running
         whenNotInUpdate();
-        if ( mode == 0 )
+        if (mode == 0)
         {
             HorseSound.Stop();
-            Invoke("onScreenRunner",3f);
+            Invoke("onScreenRunner", 3f);
         }
         else
-        {                   
+        {
             Invoke("onScreenRunner", 1f);
             Invoke("PlayHorseMusicLately", 1f);
-        }                
+        }
     }
 
     private void PlayHorseMusicLately()
     {
-        if ( !XStatics.isFullMute)
+        if (!XStatics.isFullMute)
         {
             HorseSound.Play();
-        }        
+        }
     }
 
-    public void Die( int ina2 )
-    {        
+    public void Die(int ina2)
+    {
         if (is_alive)
         {
             mode = 3;//DEATH        
@@ -133,7 +132,7 @@ public class JLife : MonoBehaviour
                 postMode = miniTypes[mode];
             }
             anim.SetInteger(controlParam, (mode * 10) + postMode);
-            whenNotInUpdate();                       
+            whenNotInUpdate();
         }
         if (ina2 == 1)// full death
         {
@@ -143,15 +142,15 @@ public class JLife : MonoBehaviour
 
     public void deathDecaying()
     {
-        decay--;        
+        decay--;
         if (decay < 0)
         {
             Destroy(gameObject);
         }
     }
-    
-    public float DNewRotAngle = 180 ;// to deny the continuity check initially
-    public int interestAngle = 180;    
+
+    public float DNewRotAngle = 180;// to deny the continuity check initially
+    public int interestAngle = 180;
     public bool isContinuity = false;
 
     public void rotationFunction(int _dx, int _dy, int mR)
@@ -164,15 +163,15 @@ public class JLife : MonoBehaviour
         //DNewRotAngle = rotAngle < 0 ? -rotAngle : rotAngle;        
         DNewRotAngle = rotAngle;
         interestAngle = ((9 * 360) - (int)DNewRotAngle) % 360;
-        if (mR != 1 || 
-        ( ! ((40 < interestAngle && interestAngle < 50) || (310 < interestAngle && interestAngle < 320)) ) )
+        if (mR != 1 ||
+        (!((40 < interestAngle && interestAngle < 50) || (310 < interestAngle && interestAngle < 320))))
         {
             transform.Rotate(new Vector3(0, rotAngle, 0));
             isContinuity = false;
         }
         else
         {
-            if ( 310 < interestAngle && interestAngle < 320 )
+            if (310 < interestAngle && interestAngle < 320)
             {
                 //DNewRotAngle = -DNewRotAngle;
                 if (DNewRotAngle < 0)
@@ -187,54 +186,54 @@ public class JLife : MonoBehaviour
             }
             else// 40 to 50
             {
-                if ( DNewRotAngle < -360 )
+                if (DNewRotAngle < -360)
                 {
                     DNewRotAngle = 360 + DNewRotAngle;
                     //print("?c" + DNewRotAngle);
-                }                
+                }
                 else
                 {
                     //print("?" + DNewRotAngle);
-                }                
+                }
             }
             isContinuity = true;
-        }        
+        }
         healthBarLookAtCamera();
     }
 
     public void movementFunction()// The Walk Checker
-    {                
-        if ( mode == 1 && _nx != -1 && _ny != -1 && (!(_nx == _x && _ny == _y)))
+    {
+        if (mode == 1 && _nx != -1 && _ny != -1 && (!(_nx == _x && _ny == _y)))
         {
             float diagFactor = 1;
-            if ( (((_nx - _x )*(_nx - _x)) + ((_ny - _y)*(_ny - _y))) == 2 )
+            if ((((_nx - _x) * (_nx - _x)) + ((_ny - _y) * (_ny - _y))) == 2)
             {// diagonal movement
                 diagFactor = ZCode.ZROOT2;
             }
             float moveFactor = 1f;// earlier 0.9f
-            if ( _wid == 3 )
+            if (_wid == 3)
             {
                 moveFactor = 2f;// earlier 1.9f
             }
-            Vector3 dest = new Vector3( _nx * distBtwTroops , height , _ny * distBtwTroops );
-            Vector3 curr = transform.position ;
+            Vector3 dest = new Vector3(_nx * distBtwTroops, height, _ny * distBtwTroops);
+            Vector3 curr = transform.position;
             Vector3 d2mini = dest - curr;
             float zimmick;
             //zimmick = 2.5f;
             zimmick = distBtwTroops;
-            if (d2mini.magnitude > 0.1f)            
+            if (d2mini.magnitude > 0.1f)
             {
                 float moveDistance = zimmick * moveFactor * Time.deltaTime;
-                transform.Translate(new Vector3(moveDistance, 0, 0));                
+                transform.Translate(new Vector3(moveDistance, 0, 0));
             }
             else
-            {                
-                suddenModelHalt();   // For halting the unit after reaching the point             
+            {
+                suddenModelHalt(1);   // For halting the unit after reaching the point                     
             }
-            if ( isContinuity )
+            if (isContinuity)
             {
                 transform.Rotate(new Vector3(0, 2 * DNewRotAngle * Time.deltaTime, 0));
-                healthBarLookAtCamera();                
+                healthBarLookAtCamera();
             }
             // for performance can this be inside of magnitude above if
         }
@@ -262,7 +261,7 @@ public class JLife : MonoBehaviour
 
         if (_packet[8] == 1)// CRITICAL UPDATE - 1 is the normal update - deemed to be the modeImplement 0 
         {
-            transform.position = new Vector3(distBtwTroops * _x, height, distBtwTroops * _y);            
+            transform.position = new Vector3(distBtwTroops * _x, height, distBtwTroops * _y);
             _gid = _packet[9];
             _isTakingArrowDamage = _packet[10];
             modeImplement(0);
@@ -271,38 +270,38 @@ public class JLife : MonoBehaviour
         else
         {
             modeImplement(1);//Immediately Implementing Mode
-        }        
+        }
     }
 
-    public void modeImplement( int _skip )
+    public void modeImplement(int _skip)
     {
         if (is_alive)
         {
             #region DefaultActions
 
-            if ( _wid == 1 )// only for shielded units later swordsmen
+            if (_wid == 1)// only for shielded units later swordsmen
             {
-                if ( _isTakingArrowDamage == 1 )
+                if (_isTakingArrowDamage == 1)
                 {
-                    anim.SetInteger(controlParam, 4 );//0+4 for idle guard action  
+                    anim.SetInteger(controlParam, 4);//0+4 for idle guard action  
                     whenNotInUpdate();
                     _isTakingArrowDamage = 0;
                     return;
                 }
             }
-            if ( mode == 0 )
-            {                
-                if ( _pid > 0 )
+            if (mode == 0)
+            {
+                if (_pid > 0)
                 {
-                    if ( _pid % 2 == 0 )
+                    if (_pid % 2 == 0)
                     {
                         rotationFunction(_x - 1, _y, mode);
                     }
                     else
                     {
                         rotationFunction(_x + 1, _y, mode);
-                    }                    
-                }                
+                    }
+                }
             }
             if (_pid > 0 && instantColoring)// When Donated to other player units change color
             {
@@ -310,7 +309,7 @@ public class JLife : MonoBehaviour
                 {
                     this.transform.Find("Mesh_Pike").GetComponent<SkinnedMeshRenderer>().material.color = pColors[_pid];
                     do_once = false;// will remove this code and also manage selection rings if the donation of army is not an idea
-                }                
+                }
                 ManageSelectionRings();
             }
 
@@ -320,43 +319,43 @@ public class JLife : MonoBehaviour
                 mode = 0;// may be this is the ghost movement like object moving but legs straight
                 // in skip:0 and countUp%3:1 i think i debugged the idle mode circle , check in new update
                 // check with a print statement if this case is coming after aug9
-            }            
-            
+            }
+
             int postMode = Random.Range(1, miniTypes[mode] + 1);
             if (postMode > miniTypes[mode])
             {
                 postMode = miniTypes[mode];
-            }                        
+            }
 
             #endregion
 
             #region Decision Anims
 
-            if ( _skip == 0 )
-            {      
-                adjustedAttackSeperation = false ;
+            if (_skip == 0)
+            {
+                adjustedAttackSeperation = false;
             }
             else
-            {                                
-                if ( mode == 0 )
-                {                    
-                    suddenModelHalt();                                        
+            {
+                if (mode == 0)
+                {
+                    suddenModelHalt( postMode );
                 }
                 else
                 {
                     anim.SetInteger(controlParam, (mode * 10) + postMode);
                     whenNotInUpdate();
                     sudIdleCounter = 0;
-                }                
+                }
 
-                if ( mode == 2 && !adjustedAttackSeperation)
+                if (mode == 2 && !adjustedAttackSeperation)
                 {// for archer ..write for diff units basing on seperation dist
-                    if ( _wid == 2 )
+                    if (_wid == 2)
                     {
-                        transform.Translate( new Vector3( distBtwTroops / 4 , 0 , 0 ));
+                        transform.Translate(new Vector3(distBtwTroops / 4, 0, 0));
                         adjustedAttackSeperation = true;
                     }
-                    if ( _wid == 1 )
+                    if (_wid == 1)
                     {
                         transform.Translate(new Vector3(distBtwTroops / 4, 0, 0));//0.75
                         adjustedAttackSeperation = true;
@@ -370,11 +369,11 @@ public class JLife : MonoBehaviour
 
     #region PostJ
 
-    public void showHealthBar( int[] _data )// Helper Octoman
+    public void showHealthBar(int[] _data)// Helper Octoman
     {
         if (_data[1] > 0)// I used brilliant skip , when max health == 0 means only to look at camera
         {
-            if ( _data[0] < cHealth )
+            if (_data[0] < cHealth)
             {
                 cHealth = _data[0];
             }
@@ -384,7 +383,7 @@ public class JLife : MonoBehaviour
                 return;
             }
             float health;
-            if ( cHealth <= 0)
+            if (cHealth <= 0)
             {
                 health = 0f;
             }
@@ -394,24 +393,25 @@ public class JLife : MonoBehaviour
             }
             if (health < 0.40f)
             {
-                healthDisplay.GetComponent<Image>().color = Color.red;
+                healthDisplay.GetComponent<SpriteRenderer>().color = Color.red;
             }
             else if (health < 0.70f)
             {
-                healthDisplay.GetComponent<Image>().color = Color.yellow;
+                healthDisplay.GetComponent<SpriteRenderer>().color = Color.yellow;
             }
             else
             {
-                healthDisplay.GetComponent<Image>().color = Color.green;
+                healthDisplay.GetComponent<SpriteRenderer>().color = Color.green;
             }
-            healthDisplay.localScale = new Vector3(health, 1f, 1f);// health = 0f....1f                            
+            healthDisplay.localScale = new Vector3(health * 0.181f, 1f * 0.091f, 1f * 0.147f );// health = 0f....1f                            
+            // the above hand coded multipliers are the default scale of healthbar
         }
         else if (_data[1] == 0)
         {
             healthBarLookAtCamera();
         }
         else
-        {     
+        {
             //special case of cHealth and cMaxHealth updates : move above only in this case      
             //healthDisplay.localScale = new Vector3(0f, 1f, 1f);
         }
@@ -419,32 +419,32 @@ public class JLife : MonoBehaviour
 
     public void changeAlphaForHealthBar()
     {
-        Color k23 = healthDisplay.GetComponent<Image>().color;
+        Color k23 = healthDisplay.GetComponent<SpriteRenderer>().color;
         k23.a = 255;//actually 255
-        healthDisplay.GetComponent<Image>().color = k23;
+        healthDisplay.GetComponent<SpriteRenderer>().color = k23;
     }
 
     public void healthBarLookAtCamera()
     {
-        healthCanvas.LookAt(healthCanvas.position + (myCamera.transform.rotation * Vector3.back),
+        healthDisplay.LookAt(healthDisplay.position + (myCamera.transform.rotation * Vector3.back),
             myCamera.transform.rotation * Vector3.up
             );
     }
 
-    public void suddenModelHalt()
+    public void suddenModelHalt( int _postMode )
     {
         sudIdleCounter++;
-        if (sudIdleCounter%3==1)
-        {
-            anim.SetInteger(controlParam, (0 * 10) + 1);//01              
+        if (sudIdleCounter % 3 == 1)
+        {            
+            anim.SetInteger(controlParam, (0 * 10) + _postMode );//01              
             whenNotInUpdate();
             sudIdleCounter = 1; //Similar to 4Mod3 logic modImp skip:0 mode:0 case
-        }        
+        }
     }
 
     public void ManageSelectionRings()
     {
-        if (ZCode.ZSelectedGID > 0 && ZCode.ZSelectedGID == _gid )
+        if (ZCode.ZSelectedGID > 0 && ZCode.ZSelectedGID == _gid)
         {
             RoundRings.SetActive(true);
             //this.transform.Find("Mesh_Pike").
@@ -455,7 +455,7 @@ public class JLife : MonoBehaviour
         {
             RoundRings.SetActive(false);
         }
-    }    
+    }
 
     public void set99()
     {
@@ -464,7 +464,7 @@ public class JLife : MonoBehaviour
 
     public void whenNotInUpdate()
     {
-        Invoke("set99", 0.01f);              
+        Invoke("set99", 0.01f);
     }
 
     public void TimeUp()
